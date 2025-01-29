ad1b30e85838851fe12761bc32bc2ec82b9a4b8b
sophiefe12 <144323222+sophiefe12@users.noreply.github.com>
2025-01-29 19:17:23 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,9 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-output "instance_ami" {
-  value = aws_instance.ubuntu.ami
+output "aws_public_ip" {
+  description = "Public IP of the AWS instance"
+  value       = aws_instance.aws_vm.public_ip
 }
 
-output "instance_arn" {
-  value = aws_instance.ubuntu.arn
+output "gcp_external_ip" {
+  description = "External IP of the GCP instance"
+  value       = google_compute_instance.gcp_vm.network_interface[0].access_config[0].nat_ip
 }
-
