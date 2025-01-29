f1ca69c405903953d7f43913553668bc08e3ab9d
sophiefe12 <144323222+sophiefe12@users.noreply.github.com>
2025-01-29 19:15:10 +0000

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,18 +1,14 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
+      version = "~> 5.0"
     }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.0"
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
     }
   }
-
-  required_version = ">= 0.14.0"
 }
