03cc78a6134d0378727d243f16709000e802ce7b
sophiefe12 <144323222+sophiefe12@users.noreply.github.com>
2025-01-29 20:50:42 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,24 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 4.0"
     }
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0"
+      version = "~> 4.0"
     }
   }
 }
-
-provider "aws" {
-  region     = var.AWS_REGION
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-}
-
-provider "google" {
-  project     = var.GCP_PROJECT_ID
-  region      = var.GCP_REGION
-  credentials = var.GOOGLE_CREDENTIALS
-}
-
