961fa03c5ca4dd0a41c1e2e141a4f62fcd98f1f6
sophiefe12 <144323222+sophiefe12@users.noreply.github.com>
2025-01-29 20:45:46 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,3 @@
-variable "AWS_ACCESS_KEY_ID" {
-  type = string
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  type = string
-}
-
-variable "AWS_REGION" {
-  type = string
-}
-
 variable "GCP_PROJECT_ID" {
   type = string
 }
@@ -19,5 +7,17 @@ variable "GCP_REGION" {
 }
 
 variable "GOOGLE_CREDENTIALS" {
+  type = string
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
+
+variable "AWS_REGION" {
   type = string
 }
