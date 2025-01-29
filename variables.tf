8ab2fd2d03a3f3b91efc8013a8c91750b4a3a124
sophiefe12 <144323222+sophiefe12@users.noreply.github.com>
2025-01-29 19:53:02 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,18 +5,15 @@ variable "aws_region" {
 }
 
 variable "GCP_PROJECT_ID" {
-  description = "GCP Project ID"
   type        = string
 }
 
 variable "GCP_REGION" {
-  description = "GCP region"
   type        = string
   default     = "us-central1"
 }
 
 variable "GOOGLE_CREDENTIALS" {
-  description = "GCP credentials JSON (stored in Terraform Cloud)"
   type        = string
   sensitive   = true
 }
