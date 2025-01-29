5c3001332665a83bec4ab9fd25124fd6415045f6
sophiefe12 <144323222+sophiefe12@users.noreply.github.com>
2025-01-29 19:16:50 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,33 +5,11 @@ terraform {
       name = "your-workspace-name"
     }
   }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-  }
-}
-
-# AWS Provider Configuration
-provider "aws" {
-  region = "us-east-1"
-}
-
-# GCP Provider Configuration
-provider "google" {
-  project = "your-gcp-project-id"
-  region  = "us-central1"
 }
 
 # AWS EC2 Instance
 resource "aws_instance" "aws_vm" {
-  ami           = "ami-0c55b159cbfafe1f0" # Ubuntu 20.04 in us-east-1
+  ami           = "ami-0c55b159cbfafe1f0"  # Ubuntu 20.04 in us-east-1
   instance_type = "t2.micro"
 
   tags = {
@@ -43,7 +21,7 @@ resource "aws_instance" "aws_vm" {
 resource "google_compute_instance" "gcp_vm" {
   name         = "terraform-gcp"
   machine_type = "e2-micro"
-  zone         = "us-central1-a"
+  zone         = "${var.gcp_region}-a"
 
   boot_disk {
     initialize_params {
