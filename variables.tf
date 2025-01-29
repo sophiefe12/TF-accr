bdec580b0f2e14b6510d3a0338a2862d99b1f881
sophiefe12 <144323222+sophiefe12@users.noreply.github.com>
2025-01-29 19:56:31 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,16 +4,7 @@ variable "AWS_REGION" {
   default     = "us-east-1"
 }
 
-variable "GCP_PROJECT_ID" {
-  type        = string
-}
-
 variable "GCP_REGION" {
   type        = string
   default     = "us-central1"
-}
-
-variable "GOOGLE_CREDENTIALS" {
-  type        = string
-  sensitive   = true
 }
