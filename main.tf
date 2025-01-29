82959193db9ed1170f554d6628ac9ff836110ee0
sophiefe12 <144323222+sophiefe12@users.noreply.github.com>
2025-01-29 21:36:24 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5"  # Use the appropriate AWS provider version
     }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"  # Use the appropriate GCP provider version
+    }
   }
 
   # Specify the minimum Terraform version required
@@ -21,13 +25,43 @@ terraform {
 
 # Configure the AWS provider
 provider "aws" {
-  region = var.aws_region  # Ensure this variable is defined in variables.tf
+  region     = var.AWS_REGION
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
-# Call the AWS instance module
-module "aws_instance" {
-  source         = "./modules/aws_instance"  # Path to your AWS instance module
-  ec2_ami        = var.aws_ami               # Pass the AMI variable to the module
-  ec2_region     = var.aws_region            # Pass the region variable to the module
-  instance_name  = var.aws_name              # Pass the instance name variable to the module
+# Configure the GCP provider
+provider "google" {
+  project     = var.GCP_PROJECT_ID
+  region      = var.GCP_REGION
+  credentials = var.GOOGLE_CREDENTIALS
+}
+
+# AWS EC2 Instance
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Replace with a valid AMI ID for your region
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "example-instance"
+  }
+}
+
+# GCP Compute Instance
+resource "google_compute_instance" "example" {
+  name         = "example-instance"
+  machine_type = "e2-micro"
+  zone         = "${var.GCP_REGION}-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"  # Replace with a valid image for your project
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
 }
