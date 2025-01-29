f4f9a3b1ca3f5b80e1163431805cf5b49028954b
sophiefe12 <144323222+sophiefe12@users.noreply.github.com>
2025-01-29 20:50:26 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,17 @@
-output "aws_vm_public_ip" {
-  description = "Public IP of the AWS VM"
-  value       = aws_instance.aws_vm.public_ip
+# AWS Outputs
+output "aws_instance_id" {
+  value = aws_instance.example.id
 }
 
-output "gcp_vm_public_ip" {
-  description = "Public IP of the GCP VM"
-  value       = google_compute_instance.gcp_vm.network_interface.0.access_config.0.nat_ip
+output "aws_instance_public_ip" {
+  value = aws_instance.example.public_ip
+}
+
+# GCP Outputs
+output "gcp_instance_id" {
+  value = google_compute_instance.example.id
+}
+
+output "gcp_instance_public_ip" {
+  value = google_compute_instance.example.network_interface[0].access_config[0].nat_ip
 }
