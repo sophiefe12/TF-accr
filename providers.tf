3cde4f56cc2149f5ffb8e2aed0c7f3a410c566bd
sophiefe12 <144323222+sophiefe12@users.noreply.github.com>
2025-01-29 19:19:28 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,11 +1,9 @@
 provider "aws" {
-  region                  = var.aws_region
-  access_key              = var.aws_access_key
-  secret_key              = var.aws_secret_key
+  region = var.aws_region
 }
 
 provider "google" {
-  credentials = file(var.gcp_credentials_file)
+  credentials = var.gcp_credentials
   project     = var.gcp_project_id
   region      = var.gcp_region
 }
