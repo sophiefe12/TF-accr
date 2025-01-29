e3f8bc021ef918b62f53d1ed7a60d8f02e8b9f8a
sophiefe12 <144323222+sophiefe12@users.noreply.github.com>
2025-01-29 21:37:05 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,25 +1,33 @@
 # AWS Variables
 variable "AWS_ACCESS_KEY_ID" {
-  type = string
+  type        = string
+  description = "AWS Access Key ID"
 }
 
 variable "AWS_SECRET_ACCESS_KEY" {
-  type = string
+  type        = string
+  description = "AWS Secret Access Key"
 }
 
 variable "AWS_REGION" {
-  type = string
+  type        = string
+  description = "AWS Region"
+  default     = "us-east-1"
 }
 
 # GCP Variables
 variable "GCP_PROJECT_ID" {
-  type = string
+  type        = string
+  description = "GCP Project ID"
 }
 
 variable "GCP_REGION" {
-  type = string
+  type        = string
+  description = "GCP Region"
+  default     = "us-central1"
 }
 
 variable "GOOGLE_CREDENTIALS" {
-  type = string
+  type        = string
+  description = "GCP Service Account Credentials"
 }
