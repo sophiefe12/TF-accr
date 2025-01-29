b4cdba11aa351440cee68eb5a14574678febc1f6
sophiefe12 <144323222+sophiefe12@users.noreply.github.com>
2025-01-29 21:23:47 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,32 @@
+terraform {
+  cloud {
+    organization = "HC_Sophie"
+    workspaces {
+      name = "hcp-ACME"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+module "aws_instance" {
+  source         = "./modules/aws_instance"
+  ec2_ami        = var.aws_ami
+  ec2_region     = var.aws_region
+  instance_name  = var.aws_name
+}
+
 # AWS Provider Configuration
 provider "aws" {
   region = var.AWS_REGION
