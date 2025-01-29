c4dfce95bb37fe09c5fb4df397be6266b4fb63dd
sophiefe12 <144323222+sophiefe12@users.noreply.github.com>
2025-01-29 20:36:51 +0000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
+
+variable "AWS_REGION" {
+  type = string
+}
+
+variable "GCP_PROJECT_ID" {
+  type = string
+}
+
+variable "GCP_REGION" {
+  type = string
+}
+
+variable "GOOGLE_CREDENTIALS" {
+  type = string
+}
