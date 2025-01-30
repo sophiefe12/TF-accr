331a1630ae9b04e6de9a81ff9f26f4830409cade
sophiefe12 <144323222+sophiefe12@users.noreply.github.com>
2025-01-30 20:12:39 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # AWS EC2 Instance
 resource "aws_instance" "aws_vm" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux AMI (Change as needed)
+  ami           = "ami-000d95aa3e1de7cc7" # Amazon Linux AMI (Change as needed)
   instance_type = "t2.micro"
 
   tags = {
