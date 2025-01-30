0314d5f8167ad6f29fff15a9b538a20822789c38
sophiefe12 <144323222+sophiefe12@users.noreply.github.com>
2025-01-30 06:08:15 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# AWS EC2 Instance
+ # AWS EC2 Instance
 resource "aws_instance" "aws_vm" {
   ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux AMI (Change as needed)
   instance_type = "t2.micro"
@@ -12,7 +12,7 @@ resource "aws_instance" "aws_vm" {
 resource "google_compute_instance" "gcp_vm" {
   name         = "gcp-vm"
   machine_type = "f1-micro"
-  zone         = "${var.GCP_REGION}-a"
+  zone         = "${GCP_REGION}-a"
 
   boot_disk {
     initialize_params {
