2867be2bef3a9f5f1e5ea09004f96cf4106898e1
sophiefe12 <144323222+sophiefe12@users.noreply.github.com>
2025-01-30 15:00:00 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "google_compute_instance" "gcp_vm" {
   name         = "gcp-vm"
   machine_type = "f1-micro"
   zone         = "${var.gcp_region}-a"
-  project      = env("GCP_PROJECT_ID")
+  project      = var.gcp_project_id
 
   boot_disk {
     initialize_params {
