ed874bad4bf9472119b2f38e06d537b652e63919
sophiefe12 <sophie.federspieler@hashicorp.com>
2025-04-25 15:29:43 +0200

    adding variables

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 # AWS EC2 Instance
 resource "aws_instance" "aws_vm" {
   ami           = "ami-0c02fb55956c7d316" # Amazon Linux AMI (Change as needed)
-  instance_type = "t2.small"
+  instance_type = var.instance_type
 
   tags = {
     Name = "AWS-VM"
   }
 }
+
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  description = "The AWS region to deploy the resources in"
+  type        = string
+  default     = "us-east-1"
+}
+variable "instance_type" {
+  description = "The type of EC2 instance to create"
+  type        = string
+  default     = "t2.small"
+}
