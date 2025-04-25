13d42f37a8eb4b43ae682860b98717851833cabc
sophiefe12 <sophie.federspieler@hashicorp.com>
2025-04-25 16:21:34 +0200

    Outputs

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "aws_instance_id" {
 output "aws_instance_public_ip" {
   value = aws_instance.aws_vm.public_ip
 }
+
+output "web_app_url" {
+  value = "http://${aws_instance.aws_vm.public_ip}"
+}
